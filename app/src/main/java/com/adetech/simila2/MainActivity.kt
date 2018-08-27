9727d85aaade865cd61c45285cfd1838910ab5dd
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2018-08-27 12:47:38 +0100

    Drafterd basic artist list ui.

--- app/src/main/java/com/adetech/simila2/MainActivity.kt
+++ app/src/main/java/com/adetech/simila2/MainActivity.kt
@@ -10,15 +10,15 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.artist_holder_item)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
